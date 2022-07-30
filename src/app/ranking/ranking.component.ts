b2fd196abe8f7e381ff6aff8b43bbe8f77df8945
comeaucar <cartercomeau@icloud.com>
2022-07-30 01:45:21 -0400

    added ranking system

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -3,6 +3,8 @@ import { Auth } from '@angular/fire/auth';
 import { Firestore } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { getDoc, doc, collection, getDocFromCache } from 'firebase/firestore';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-ranking',
@@ -18,13 +20,16 @@ export class RankingComponent implements OnInit, OnDestroy {
   }
   id: string = ''
   private sub: any
-  constructor(private route: ActivatedRoute, public auth: Auth, public firestore: Firestore) { }
+  results: any = [];
+  limitReached: boolean = false;
+  constructor(private route: ActivatedRoute, public auth: Auth, public firestore: Firestore, public snackbar: MatSnackBar) { }
 
   async ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id']
       this.getRanking().then((res) => {
         this.ranking = res
+        console.log(this.ranking)
       });
     })
   }
@@ -45,4 +50,27 @@ export class RankingComponent implements OnInit, OnDestroy {
     this.sub.unsubscribe();
   }
 
+  checkLimit(): boolean{
+    if (this.results.length >= this.ranking.rankLimit) {
+      return true;
+    }
+    return false
+  }
+
+  submitRankings() {
+    
+  }
+
+  drop(event: any) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
